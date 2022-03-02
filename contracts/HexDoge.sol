84b1a51558a1b7c47606cb2e7bd2cc8fa4c0ead7
BayMo <baymo@BayMos-MacBook-Pro-2.local>
2022-03-03 02:19:01 +0800

    define metadata for 72 genius NFT

--- contracts/HexDoge.sol
+++ contracts/HexDoge.sol
@@ -30,6 +30,7 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 contract HexDoge is Context, AccessControlEnumerable, ERC1155Burnable, ERC1155Pausable {
     struct Metadata{
       string status;
+      string category;
       uint256 DuplicateDuration;
       uint256 DuplicateSuccessRate;
       uint256 DuplicateEnergyEarn;
@@ -38,7 +39,7 @@ contract HexDoge is Context, AccessControlEnumerable, ERC1155Burnable, ERC1155Pa
     }
 
     mapping(uint256 => Metadata) public metadata;
-
+    uint256 public now_ = block.timestamp;
     string public name = "HexDoge";
 
     uint256 public constant MINT_PRICE = 1;
@@ -57,16 +58,402 @@ contract HexDoge is Context, AccessControlEnumerable, ERC1155Burnable, ERC1155Pa
         _setupRole(MINTER_ROLE, _msgSender());
         _setupRole(PAUSER_ROLE, _msgSender());
 
-        for(uint i = 1; i <= 72; i++) {
-          metadata[i] = Metadata({
+
+          metadata[1] = Metadata({
             status: 'liveness',
-            DuplicateDuration: 1,
-            DuplicateSuccessRate: 70,
-            DuplicateEnergyEarn: 200,
-            DuplicateDoneDate: 1646241741,
-            FixingEnergyCost: 100
+            category: 'genesis',
+            DuplicateDuration: 31,
+            DuplicateSuccessRate: 46,
+            DuplicateEnergyEarn: 2,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 24
           });
-        }
+    
+
+          metadata[2] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 54,
+            DuplicateSuccessRate: 54,
+            DuplicateEnergyEarn: 6,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 1
+          });
+    
+
+          metadata[3] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 21,
+            DuplicateSuccessRate: 45,
+            DuplicateEnergyEarn: 6,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 15
+          });
+    
+
+          metadata[4] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 30,
+            DuplicateSuccessRate: 47,
+            DuplicateEnergyEarn: 9,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 9
+          });
+    
+
+          metadata[5] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 42,
+            DuplicateSuccessRate: 43,
+            DuplicateEnergyEarn: 22,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 25
+          });
+    
+
+          metadata[6] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 35,
+            DuplicateSuccessRate: 57,
+            DuplicateEnergyEarn: 4,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 10
+          });
+    
+
+          metadata[7] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 57,
+            DuplicateSuccessRate: 53,
+            DuplicateEnergyEarn: 14,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 16
+          });
+    
+
+          metadata[8] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 42,
+            DuplicateSuccessRate: 49,
+            DuplicateEnergyEarn: 14,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 21
+          });
+    
+
+          metadata[9] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 31,
+            DuplicateSuccessRate: 58,
+            DuplicateEnergyEarn: 25,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 25
+          });
+    
+
+          metadata[10] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 28,
+            DuplicateSuccessRate: 40,
+            DuplicateEnergyEarn: 15,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 22
+          });
+    
+
+          metadata[11] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 31,
+            DuplicateSuccessRate: 22,
+            DuplicateEnergyEarn: 20,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 28
+          });
+    
+
+          metadata[12] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 42,
+            DuplicateSuccessRate: 28,
+            DuplicateEnergyEarn: 29,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 1
+          });
+    
+
+          metadata[13] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 30,
+            DuplicateSuccessRate: 54,
+            DuplicateEnergyEarn: 4,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 20
+          });
+    
+
+          metadata[14] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 53,
+            DuplicateSuccessRate: 40,
+            DuplicateEnergyEarn: 18,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 30
+          });
+    
+
+          metadata[15] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 43,
+            DuplicateSuccessRate: 38,
+            DuplicateEnergyEarn: 19,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 19
+          });
+    
+
+          metadata[16] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 31,
+            DuplicateSuccessRate: 41,
+            DuplicateEnergyEarn: 16,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 5
+          });
+    
+
+          metadata[17] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 35,
+            DuplicateSuccessRate: 14,
+            DuplicateEnergyEarn: 4,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 7
+          });
+    
+
+          metadata[18] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 38,
+            DuplicateSuccessRate: 46,
+            DuplicateEnergyEarn: 29,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 10
+          });
+    
+
+          metadata[19] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 46,
+            DuplicateSuccessRate: 45,
+            DuplicateEnergyEarn: 21,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 8
+          });
+    
+
+          metadata[20] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 27,
+            DuplicateSuccessRate: 31,
+            DuplicateEnergyEarn: 14,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 10
+          });
+    
+
+          metadata[21] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 56,
+            DuplicateSuccessRate: 31,
+            DuplicateEnergyEarn: 2,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 4
+          });
+    
+
+          metadata[22] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 42,
+            DuplicateSuccessRate: 46,
+            DuplicateEnergyEarn: 21,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 2
+          });
+    
+
+          metadata[23] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 24,
+            DuplicateSuccessRate: 60,
+            DuplicateEnergyEarn: 25,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 12
+          });
+    
+
+          metadata[24] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 45,
+            DuplicateSuccessRate: 43,
+            DuplicateEnergyEarn: 9,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 10
+          });
+    
+
+          metadata[25] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 52,
+            DuplicateSuccessRate: 25,
+            DuplicateEnergyEarn: 5,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 14
+          });
+    
+
+          metadata[26] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 51,
+            DuplicateSuccessRate: 55,
+            DuplicateEnergyEarn: 18,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 14
+          });
+    
+
+          metadata[27] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 26,
+            DuplicateSuccessRate: 11,
+            DuplicateEnergyEarn: 22,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 2
+          });
+    
+
+          metadata[28] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 25,
+            DuplicateSuccessRate: 34,
+            DuplicateEnergyEarn: 21,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 29
+          });
+    
+
+          metadata[29] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 28,
+            DuplicateSuccessRate: 24,
+            DuplicateEnergyEarn: 3,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 12
+          });
+    
+
+          metadata[30] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 37,
+            DuplicateSuccessRate: 43,
+            DuplicateEnergyEarn: 19,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 4
+          });
+    
+
+          metadata[31] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 40,
+            DuplicateSuccessRate: 52,
+            DuplicateEnergyEarn: 3,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 28
+          });
+    
+
+          metadata[32] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 59,
+            DuplicateSuccessRate: 56,
+            DuplicateEnergyEarn: 28,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 16
+          });
+    
+
+          metadata[33] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 59,
+            DuplicateSuccessRate: 34,
+            DuplicateEnergyEarn: 11,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 7
+          });
+    
+
+          metadata[34] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 36,
+            DuplicateSuccessRate: 56,
+            DuplicateEnergyEarn: 29,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 21
+          });
+    
+
+          metadata[35] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 21,
+            DuplicateSuccessRate: 37,
+            DuplicateEnergyEarn: 9,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 20
+          });
+    
+
+          metadata[36] = Metadata({
+            status: 'liveness',
+            category: 'genesis',
+            DuplicateDuration: 20,
+            DuplicateSuccessRate: 58,
+            DuplicateEnergyEarn: 26,
+            DuplicateDoneDate: 0,
+            FixingEnergyCost: 29
+          });
+    
     }
 
     /**
