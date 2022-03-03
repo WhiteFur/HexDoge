edde0fbaa2d858022ab69c68b93fa459ca724c9a
abner.huang <abner.huang@titansoft.com.sg>
2022-03-03 10:42:02 +0800

    update

--- contracts/HexDoge.sol
+++ contracts/HexDoge.sol
@@ -6,11 +6,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
-import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/Strings.sol"; 
-
 
 /**
  * @dev {ERC1155} token, including:
@@ -28,7 +26,7 @@ import "@openzeppelin/contracts/utils/Strings.sol";
  *
  * _Deprecated in favor of https://wizard.openzeppelin.com/[Contracts Wizard]._
  */
-contract HexDoge is Context, AccessControlEnumerable, ERC1155Burnable, ERC1155Pausable, ERC1155Holder {
+contract HexDoge is Context, AccessControlEnumerable, ERC1155Burnable, ERC1155Pausable {
     struct Metadata{
       string status;
       string category;
@@ -43,11 +41,21 @@ contract HexDoge is Context, AccessControlEnumerable, ERC1155Burnable, ERC1155Pa
     uint256 public now_ = block.timestamp;
     string public name = "HexDoge";
 
-    uint256 public constant MINT_PRICE = 1;
+    uint256 public constant MINT_PRICE = 0;
     uint256 public constant MAX_TOKEN_ID = 36;
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    uint256 public CURRENT_GENESIS_ID = 0;
+    uint256 public CURRENT_NORMAL_ID = 0;
+    uint256 constant MIN_HEX_GENESIS_ID = 1;
+    uint256 constant MAX_HEX_GENESIS_ID = 36;
+    uint256 constant MIN_HEX_NORMAL_ID = 1000;
+    uint256 constant MAX_HEX_NORMAL_ID = 9000;
+    uint256 constant ENERGY_TOKEN_ID = 9999;
+    uint256 constant ENERGY_TOKEN_MAX_SUPPLY = 1000000000;
+
+    mapping(uint256 => address) public token2ownerMap;
 
     /**
      * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE`, and `PAUSER_ROLE` to the account that
@@ -58,403 +66,54 @@ contract HexDoge is Context, AccessControlEnumerable, ERC1155Burnable, ERC1155Pa
 
         _setupRole(MINTER_ROLE, _msgSender());
         _setupRole(PAUSER_ROLE, _msgSender());
+        _mint(msg.sender, ENERGY_TOKEN_ID, ENERGY_TOKEN_MAX_SUPPLY, "");
+    }
 
+    function generateGenesisHex(uint256 tokenId, uint256 randomNumber) private returns (Metadata storage) {
+        require(tokenId >= MIN_HEX_GENESIS_ID);
+        require(tokenId <= MAX_HEX_GENESIS_ID);
 
-          metadata[1] = Metadata({
+        metadata[tokenId] = Metadata({
             status: 'liveness',
             category: 'genesis',
-            DuplicateDuration: 31,
-            DuplicateSuccessRate: 46,
-            DuplicateEnergyEarn: 2,
+            DuplicateDuration: randomNumber % 100,
+            DuplicateSuccessRate: randomNumber % 100,
+            DuplicateEnergyEarn: randomNumber * 1000,
             DuplicateDoneDate: 0,
-            FixingEnergyCost: 24
-          });
-    
+            FixingEnergyCost: randomNumber * 100
+        });
 
-          metadata[2] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 54,
-            DuplicateSuccessRate: 54,
-            DuplicateEnergyEarn: 6,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 1
-          });
-    
+        return metadata[tokenId];
+    }
 
-          metadata[3] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 21,
-            DuplicateSuccessRate: 45,
-            DuplicateEnergyEarn: 6,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 15
-          });
-    
+    function generateNormalHex(uint256 tokenId, uint256 randomNumber) private returns (Metadata storage) {
+        require(tokenId >= MIN_HEX_NORMAL_ID);
+        require(tokenId <= MAX_HEX_NORMAL_ID);
 
-          metadata[4] = Metadata({
+        metadata[tokenId] = Metadata({
             status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 30,
-            DuplicateSuccessRate: 47,
-            DuplicateEnergyEarn: 9,
+            category: 'normal',
+            DuplicateDuration: randomNumber % 100,
+            DuplicateSuccessRate: randomNumber % 100,
+            DuplicateEnergyEarn: randomNumber * 1000,
             DuplicateDoneDate: 0,
-            FixingEnergyCost: 9
-          });
-    
+            FixingEnergyCost: randomNumber * 100
+        });
 
-          metadata[5] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 42,
-            DuplicateSuccessRate: 43,
-            DuplicateEnergyEarn: 22,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 25
-          });
-    
+        return metadata[tokenId];
+    }
 
-          metadata[6] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 35,
-            DuplicateSuccessRate: 57,
-            DuplicateEnergyEarn: 4,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 10
-          });
-    
-
-          metadata[7] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 57,
-            DuplicateSuccessRate: 53,
-            DuplicateEnergyEarn: 14,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 16
-          });
-    
-
-          metadata[8] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 42,
-            DuplicateSuccessRate: 49,
-            DuplicateEnergyEarn: 14,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 21
-          });
-    
-
-          metadata[9] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 31,
-            DuplicateSuccessRate: 58,
-            DuplicateEnergyEarn: 25,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 25
-          });
-    
-
-          metadata[10] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 28,
-            DuplicateSuccessRate: 40,
-            DuplicateEnergyEarn: 15,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 22
-          });
-    
-
-          metadata[11] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 31,
-            DuplicateSuccessRate: 22,
-            DuplicateEnergyEarn: 20,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 28
-          });
-    
-
-          metadata[12] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 42,
-            DuplicateSuccessRate: 28,
-            DuplicateEnergyEarn: 29,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 1
-          });
-    
-
-          metadata[13] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 30,
-            DuplicateSuccessRate: 54,
-            DuplicateEnergyEarn: 4,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 20
-          });
-    
-
-          metadata[14] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 53,
-            DuplicateSuccessRate: 40,
-            DuplicateEnergyEarn: 18,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 30
-          });
-    
-
-          metadata[15] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 43,
-            DuplicateSuccessRate: 38,
-            DuplicateEnergyEarn: 19,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 19
-          });
-    
-
-          metadata[16] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 31,
-            DuplicateSuccessRate: 41,
-            DuplicateEnergyEarn: 16,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 5
-          });
-    
-
-          metadata[17] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 35,
-            DuplicateSuccessRate: 14,
-            DuplicateEnergyEarn: 4,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 7
-          });
-    
-
-          metadata[18] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 38,
-            DuplicateSuccessRate: 46,
-            DuplicateEnergyEarn: 29,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 10
-          });
-    
-
-          metadata[19] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 46,
-            DuplicateSuccessRate: 45,
-            DuplicateEnergyEarn: 21,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 8
-          });
-    
-
-          metadata[20] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 27,
-            DuplicateSuccessRate: 31,
-            DuplicateEnergyEarn: 14,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 10
-          });
-    
-
-          metadata[21] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 56,
-            DuplicateSuccessRate: 31,
-            DuplicateEnergyEarn: 2,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 4
-          });
-    
-
-          metadata[22] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 42,
-            DuplicateSuccessRate: 46,
-            DuplicateEnergyEarn: 21,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 2
-          });
-    
-
-          metadata[23] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 24,
-            DuplicateSuccessRate: 60,
-            DuplicateEnergyEarn: 25,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 12
-          });
-    
-
-          metadata[24] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 45,
-            DuplicateSuccessRate: 43,
-            DuplicateEnergyEarn: 9,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 10
-          });
-    
-
-          metadata[25] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 52,
-            DuplicateSuccessRate: 25,
-            DuplicateEnergyEarn: 5,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 14
-          });
-    
-
-          metadata[26] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 51,
-            DuplicateSuccessRate: 55,
-            DuplicateEnergyEarn: 18,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 14
-          });
-    
-
-          metadata[27] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 26,
-            DuplicateSuccessRate: 11,
-            DuplicateEnergyEarn: 22,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 2
-          });
-    
-
-          metadata[28] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 25,
-            DuplicateSuccessRate: 34,
-            DuplicateEnergyEarn: 21,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 29
-          });
-    
-
-          metadata[29] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 28,
-            DuplicateSuccessRate: 24,
-            DuplicateEnergyEarn: 3,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 12
-          });
-    
-
-          metadata[30] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 37,
-            DuplicateSuccessRate: 43,
-            DuplicateEnergyEarn: 19,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 4
-          });
-    
-
-          metadata[31] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 40,
-            DuplicateSuccessRate: 52,
-            DuplicateEnergyEarn: 3,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 28
-          });
-    
-
-          metadata[32] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 59,
-            DuplicateSuccessRate: 56,
-            DuplicateEnergyEarn: 28,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 16
-          });
-    
-
-          metadata[33] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 59,
-            DuplicateSuccessRate: 34,
-            DuplicateEnergyEarn: 11,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 7
-          });
-    
-
-          metadata[34] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 36,
-            DuplicateSuccessRate: 56,
-            DuplicateEnergyEarn: 29,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 21
-          });
-    
-
-          metadata[35] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 21,
-            DuplicateSuccessRate: 37,
-            DuplicateEnergyEarn: 9,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 20
-          });
-    
-
-          metadata[36] = Metadata({
-            status: 'liveness',
-            category: 'genesis',
-            DuplicateDuration: 20,
-            DuplicateSuccessRate: 58,
-            DuplicateEnergyEarn: 26,
-            DuplicateDoneDate: 0,
-            FixingEnergyCost: 29
-          });
-    
+    function getHexMetadata(uint256 tokenId) public view 
+    returns (string memory, uint256, uint256, uint256, uint256, uint256) {
+        return ( 
+            metadata[tokenId].status,
+        //  metadata[tokenId].category,
+            metadata[tokenId].DuplicateDuration,
+            metadata[tokenId].DuplicateSuccessRate,
+            metadata[tokenId].DuplicateEnergyEarn,
+            metadata[tokenId].DuplicateDoneDate,
+            metadata[tokenId].FixingEnergyCost
+        );
     }
 
     /**
@@ -469,13 +128,70 @@ contract HexDoge is Context, AccessControlEnumerable, ERC1155Burnable, ERC1155Pa
     function mint(
         address to,
         uint256 amount
-    ) public payable {
+    ) public payable returns (uint256[] memory) {
         // require(hasRole(MINTER_ROLE, _msgSender()), "ERC1155PresetMinterPauser: must have minter role to mint");
+        require(CURRENT_GENESIS_ID + amount < MAX_HEX_GENESIS_ID);
         require(msg.value == MINT_PRICE * amount, "not enough ETH");
+        uint256[] memory ids = new uint256[](amount);
+        
 
-        uint256 randomNumber = uint256(blockhash(block.number - 1));
-        uint256 mintTokenId = randomNumber % MAX_TOKEN_ID + 1;
-        _mint(to, mintTokenId, amount, "");
+        // ownerList[to][0] = 1;
+        // uint256 currentIndex = ownerList[to].length;
+
+        // uint256 randomNumber = uint256(blockhash(block.number - 1));
+        for(uint16 i = 0; i < amount; i++) {
+            CURRENT_GENESIS_ID += 1;
+            _mint(to, CURRENT_GENESIS_ID, 1, "");
+            generateGenesisHex(CURRENT_GENESIS_ID, 1);
+            token2ownerMap[CURRENT_GENESIS_ID] = msg.sender;
+            ids[i] = CURRENT_GENESIS_ID;
+        }
+
+        return ids;
+    }
+
+    function doDuplicate(uint16 tokenId) public returns(uint256) {
+        require(balanceOf(msg.sender, tokenId) != 0, "you are not owner");
+        require(keccak256(abi.encodePacked((metadata[tokenId].category))) == keccak256(abi.encodePacked(('genesis'))), "HEX category need to be GENESIS");
+        require(keccak256(abi.encodePacked((metadata[tokenId].status))) == keccak256(abi.encodePacked(('liveness'))), "HEX status need to be liveness");
+
+        metadata[tokenId].status = 'duplicating';
+        metadata[tokenId].DuplicateDoneDate = block.timestamp + metadata[tokenId].DuplicateDuration;
+        
+        return metadata[tokenId].DuplicateDoneDate;
+    }
+
+    function doClaimAll(uint16 tokenId) public returns(string memory) {
+        require(balanceOf(msg.sender, tokenId) != 0, "you are not owner");
+        require(keccak256(abi.encodePacked((metadata[tokenId].status))) == keccak256(abi.encodePacked(('duplicating'))), "HEX status need to be liveness");
+        require(metadata[tokenId].DuplicateDoneDate <= block.timestamp, "still in duplicating");
+
+
+        metadata[tokenId].status = 'duplicating';
+        metadata[tokenId].DuplicateDoneDate = block.timestamp + metadata[tokenId].DuplicateDuration;
+
+        uint256 randomNumber = uint256(blockhash(block.number - 1)) % 100;
+        if (metadata[tokenId].DuplicateSuccessRate < randomNumber) {
+            metadata[tokenId].status = 'liveness';
+            _mint(msg.sender, ENERGY_TOKEN_ID, metadata[tokenId].DuplicateEnergyEarn, "");
+        } else {
+            metadata[tokenId].status = 'broken';
+        }
+        metadata[tokenId].DuplicateDoneDate = 0;
+        
+        return metadata[tokenId].status;
+    }
+
+    function doFixing(uint16 tokenId) public returns(uint256) {
+        require(balanceOf(msg.sender, tokenId) != 0, "you are not owner");
+        require(balanceOf(msg.sender, ENERGY_TOKEN_ID) >= metadata[tokenId].FixingEnergyCost, "insufficient ENERGY TOKEN");
+        require(keccak256(abi.encodePacked((metadata[tokenId].category))) == keccak256(abi.encodePacked(('genesis'))), "HEX category need to be GENESIS");
+        require(keccak256(abi.encodePacked((metadata[tokenId].status))) == keccak256(abi.encodePacked(('broken'))), "HEX status need to be broken");
+
+        safeTransferFrom(msg.sender, address(this), ENERGY_TOKEN_ID, metadata[tokenId].FixingEnergyCost, "");
+        metadata[tokenId].status = 'liveness';
+        
+        return balanceOf(msg.sender, ENERGY_TOKEN_ID);
     }
 
     /**
@@ -491,10 +207,6 @@ contract HexDoge is Context, AccessControlEnumerable, ERC1155Burnable, ERC1155Pa
 
         //_mintBatch(to, ids, amounts, data);
     //}
-
-    function duplicate (uint256 id) public {
-      safeTransferFrom(_msgSender(), address(this), id, 1, "");
-    }
 
 
     function uri(uint256 tokenId) override public view 
@@ -537,7 +249,7 @@ contract HexDoge is Context, AccessControlEnumerable, ERC1155Burnable, ERC1155Pa
         public
         view
         virtual
-        override(AccessControlEnumerable, ERC1155, ERC1155Receiver)
+        override(AccessControlEnumerable, ERC1155)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
